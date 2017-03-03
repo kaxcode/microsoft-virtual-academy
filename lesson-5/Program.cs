5dd117ede979105e482c4ffca7775689de490cd9
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-03 13:07:54 -0500

    allocates space for x and y and writes a small alegebraic formula

---
+++ lesson-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Variables
+    {
+        public static void Main(string[] args)
+        {
+            int = x;
+            int = y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
