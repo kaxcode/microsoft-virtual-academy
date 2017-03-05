71ab3a441ec8dcf71f25a58eec5aeb3fc240ad98
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-05 15:12:28 -0500

    Writes a console name game

--- lesson-5/Program.cs
+++ lesson-5/Program.cs
@@ -6,14 +6,27 @@ namespace Variables
     {
         public static void Main(string[] args)
         {
-            int x;
-            int y;
+            // int x;
+            // int y;
 
-            x = 7;
-            y = x + 3;
+            // x = 7;
+            // y = x + 3;
 
-            Console.WriteLine(y);
-            Console.ReadLine();
+            // Console.WriteLine(y);
+            // Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            
+            string myFirstname;
+            myFirstname = Console.ReadLine();
+
+            string myLastname;
+            Console.Write("Type your last name: ");
+            myLastname = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstname + " " + myLastname)
+
 
         }
     }
