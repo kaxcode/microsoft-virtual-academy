e898706cf98cadc7d2f9465484eabda9f6a404af
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-06 09:00:39 -0500

    Adds more options for the giveaway game

--- lesson-6/Program.cs
+++ lesson-6/Program.cs
@@ -15,6 +15,17 @@ namespace Decisions
                 string message = "You won a new car!";
                 Console.WriteLine(message);
             }
+            else if (userValue == "2");
+            {
+                string message = "Y ou won a new boat!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3");
+            {
+                string message = "You won a new cat!";
+                Console.WriteLine(message);
+            }
+            
 
             Console.ReadLine();
         }
