3d93e9307b0958ce907446da130e796799a757b0
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-06 09:24:54 -0500

    Cleans code and crcreates a simpler if/else statement

--- lesson-6/Program.cs
+++ lesson-6/Program.cs
@@ -6,32 +6,34 @@ namespace Decisions
     {
         public static void Main(string[] args)
         {
+            // Console.WriteLine("Bob's Big Giveaway");
+            // Console.Write("Choose a door: 1, 2, or 3: ");
+            // string userValue = Console.ReadLine();
+
+            // string message = "";
+
+            // if (userValue == "1")
+            //     message = "You won a new car!";
+            // else if (userValue == "2")
+            //     message = "You won a new boat!";
+            // else if (userValue == "3")
+            //     message = "You won a new cat!";
+            // else 
+            // {
+            //     message = "Sorry, we didn't understand.";
+            //     message += " You lose.";
+            
+            // }            
+            // Console.WriteLine(message);
+            // Console.ReadLine();
+
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1, 2, or 3: ");
             string userValue = Console.ReadLine();
 
-            if (userValue == "1")
-            {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
-            else if (userValue == "2")
-            {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
-            }
-            else if (userValue == "3")
-            {
-                string message = "You won a new cat!";
-                Console.WriteLine(message);
-            }
-            else 
-            {
-                string message = "Sorry, we didn't understand.";
-                Console.WriteLine(message);
-            }
-            
-
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            // Console.Write("You won a {0}.", message);
+            Console.Write("You enetered {0}, therefore you won a {1}.", userValue, message);
             Console.ReadLine();
         }
     }
