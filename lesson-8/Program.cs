3018f08dcb3844bb8bb6091db350bf038b1129b9
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-06 10:15:44 -0500

    Creates a small for iteration program

---
+++ lesson-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForIteration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Console.WriteLine();
+                if (i == 7 )
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
