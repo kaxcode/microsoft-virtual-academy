7d8bd924b46d775f09a09945c893692551b6d17d
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-07 11:00:56 -0500

    Formats strings into numbers and helps escaped string sequences

---
+++ lesson-12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkingWithStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // String escape sequences
+
+            // string myString = "My \"so-called\" life.";
+            // string myString = "What if I need a\nnew line?";
+            // string myString = "Go to your C:\\ drive.";
+            // string myString = @"Go to your C:\ drive.";
+
+            // Formatting strings to numbers
+
+            // string myString = String.Format("{1} {0}", "First", "Second");
+            // string myString = String.Format("{0:C}", 123.45);
+            // string myString = string.Format("{0:N}", 123456789);
+            // string myString = string.Format("Percentage: {0:P}", .423);
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }  
+    }
+}
+
