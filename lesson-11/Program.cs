21fedc62c054eff786561aa3342e823296e76442
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-07 08:32:58 -0500

    Create a main menu method to prompt user for an option selection

---
+++ lesson-11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WhileIteration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option!");
+            Console.WriteLine("1) Print Numbers");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                return true;
+            }
+            else if (result == "2")
+            {
+                return true;
+            }
+            else if (result == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
