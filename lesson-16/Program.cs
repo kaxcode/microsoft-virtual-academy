63737f0ed10652a7fc2bb8c43f620d6d23e06230
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-08 13:46:24 -0500

    Initial commit

---
+++ lesson-16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnderstandingScope
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string j = "";
+            for (int i = 0; i < 10 ; i++)       
+            {
+                j = i.ToString();
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
