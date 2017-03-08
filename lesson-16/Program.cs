26a0bf24133b94ef4967cb9c8a104d9c83563dc3
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-08 13:52:40 -0500

    Creates a private field that can be accessed by all the member's of that class. In this case the Program class.

--- lesson-16/Program.cs
+++ lesson-16/Program.cs
@@ -4,14 +4,20 @@ namespace UnderstandingScope
 {
     public class Program
     {
+        // private field. Sort of like a property that is private but available to all the member's of the class.
+        private static string k = "";
+
         public static void Main(string[] args)
         {
             string j = "";
             for (int i = 0; i < 10 ; i++)       
             {
                 j = i.ToString();
+                k = i.ToString();
                 Console.WriteLine(i);
             }
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
             Console.ReadLine();
         }
     }
