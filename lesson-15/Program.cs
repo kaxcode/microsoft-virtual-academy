3f0cb5ed79a571f9326141d2e8e600950db7574a
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-08 09:38:14 -0500

    Created a Car class with properties

--- lesson-15/Program.cs
+++ lesson-15/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApplication
+namespace ObjectLifetime
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+
+            myCar.Make = "Oldmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            Console.ReadLine();
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
         }
     }
 }
